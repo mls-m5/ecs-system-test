34e4d55ec94bb131a592bd0803646b94d4228097
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-05-06 15:18:29 +0200

    Update and draw

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,51 @@ struct Position {
     Id<Position> id;
     float x = 0;
     float y = 0;
+    float vx = 0;
+    float vy = 0;
+
+    void update(double t) {
+        x += vx * t;
+        y += vy * t;
+        std::cout << "new position(" << x << ", " << y << ")\n";
+    }
 };
 
 struct MonsterSound {
     Id<MonsterSound> id;
 
     std::string sound = "grr!";
+
+    void update(double t) {
+        std::cout << "groff: " << sound << "\n";
+    }
+};
+
+struct Drawable {
+    std::string apperance;
+
+    void draw() {
+        std::cout << "draw: " << apperance << "\n";
+    }
+
+    void update(double t) {}
 };
 
 struct SystemBase {
     virtual ~SystemBase() = default;
+
+    virtual void update(double t) = 0;
 };
 
 template <typename T>
 struct System : SystemBase {
     std::vector<T> elements;
+
+    void update(double t) override {
+        for (auto &e : elements) {
+            e.update(t);
+        }
+    }
 };
 
 struct Objects {
@@ -45,7 +75,7 @@ struct Objects {
     template <typename T>
     int getId() {
         if (Id<T>::get() == -1) {
-            systems.emplace_back();
+            systems.push_back(std::make_shared<System<T>>());
             Id<T>::set(systems.size() - 1);
         }
 
@@ -62,6 +92,12 @@ struct Objects {
         auto &system = getSystem<T>();
         return system.elements.emplace_back();
     }
+
+    void update(double t) {
+        for (auto &system : systems) {
+            system->update(t);
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {
@@ -70,7 +106,24 @@ int main(int argc, char *argv[]) {
     // TODO: references will be invalid if resize happends on vector
     // Choose for example safe_ptr
     auto &position = objects.create<Position>();
+    position.x = 10;
     auto &sound = objects.create<MonsterSound>();
+    auto &drawable = objects.create<Drawable>();
+    drawable.apperance = "ugly monster";
+
+    // Wind
+    float windConstant = .01;
+    for (auto &p : objects.getSystem<Position>().elements) {
+        p.vx += windConstant;
+    }
+
+    double t = 0.1;
+    for (int i = 0; i < 10; ++i) {
+        objects.update(t);
+        for (auto &d : objects.getSystem<Drawable>().elements) {
+            d.draw();
+        }
+    }
 
     return 0;
 }
